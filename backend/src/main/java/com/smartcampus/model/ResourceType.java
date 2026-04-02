7b5ba241d78d8fc8fa9c09501ebca3367311c7eb
GAMAGE G.P.G.H.S <it23801318@my.sliit.lk>
2026-04-02 06:41:00 +0530

    Add ResourceType.java

---
+++ backend/src/main/java/com/smartcampus/model/ResourceType.java
@@ -0,0 +1,19 @@
+package com.smartcampus.model;
+
+public enum ResourceType {
+    LECTURE_HALL,
+    LAB,
+    MEETING_ROOM,
+    EQUIPMENT,
+    OUTDOOR_SPACE,
+    PARKING,
+    OTHER;
+
+    public static ResourceType fromString(String value) {
+        try {
+            return ResourceType.valueOf(value.toUpperCase().replace(" ", "_"));
+        } catch (IllegalArgumentException e) {
+            return OTHER;
+        }
+    }
+}
