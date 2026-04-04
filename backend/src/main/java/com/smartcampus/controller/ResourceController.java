f2f24eed467e0dfc0e42c3a0ce1c89e62b794c48
GAMAGE G.P.G.H.S <it23801318@my.sliit.lk>
2026-04-04 08:50:00 +0530

    Update ResourceController.java

--- backend/src/main/java/com/smartcampus/controller/ResourceController.java
+++ backend/src/main/java/com/smartcampus/controller/ResourceController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Controller for managing campus resources (e.g., rooms, equipment)
 @RestController
 @RequestMapping("/resources")
 @CrossOrigin(origins = "http://localhost:3000")
