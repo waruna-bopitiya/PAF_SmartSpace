0b057899df73b6d93c4ce0f6f10b87199f0a7a1c
GAMAGE G.P.G.H.S <it23801318@my.sliit.lk>
2026-04-07 05:30:00 +0530

    Add a comment in ResourceType file

--- backend/src/main/java/com/smartcampus/model/ResourceType.java
+++ backend/src/main/java/com/smartcampus/model/ResourceType.java
@@ -10,6 +10,7 @@ public enum ResourceType {
     PARKING,
     OTHER;
 
+    // Utility method to convert string to enum, defaults to OTHER if invalid
     public static ResourceType fromString(String value) {
         try {
             return ResourceType.valueOf(value.toUpperCase().replace(" ", "_"));
